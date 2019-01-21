dc5fdb706568b7baadb8cfa7bc9a5fc1c9925e0e
Linmiao Xu <linmiao.xu@gmail.com>
2019-01-21 16:59:33 +0800

    Add more text explanations

--- app/code/data_structures/disjoint_set/s2.rb
+++ app/code/data_structures/disjoint_set/s2.rb
@@ -2,13 +2,21 @@ class DisjointSet
 
   def initialize(size)
     @parents = (0 .. (size - 1)).to_a
+    @ranks = Array.new(size, 0)
   end
 
   def union(x, y)
     fx = find(x)
     fy = find(y)
     return if fx == fy
-    @parents[fx] = fy
+    if @ranks[fx] < @ranks[fy]
+      @parents[fy] = fx
+    elsif @ranks[fx] > @ranks[fy]
+      @parents[fx] = fy
+    else
+      @parents[fx] = fy
+      @rank[fy] += 1
+    end
   end
 
   def find(x)
--- app/code/sorts/radix_sort.rb
+++ app/code/sorts/radix_sort.rb
@@ -1,5 +1,4 @@
-def counting_sort(values, digit)
-  base = 10
+def counting_sort(values, base, digit)
   counts = Array.new(base, 0)
   values.each do |value|
     counts[value/(base**digit) % base] += 1
@@ -19,10 +18,10 @@ def counting_sort(values, digit)
   output
 end
 
-def radix_sort(values)
-  n = values.max.digits.count
+def radix_sort(values, base = 10)
+  n = values.max.to_s(base).length
   (0 .. n-1).each do |d|
-    values = counting_sort(values, d)
+    values = counting_sort(values, base, d)
   end
   values
 end
