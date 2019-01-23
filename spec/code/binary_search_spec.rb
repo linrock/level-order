16eb6ec8255d9f0d1046462a641c4413b66c657a
Linmiao Xu <linmiao.xu@gmail.com>
2019-01-23 14:28:52 +0800

    Compare binary search results to native ruby implementation

--- spec/code/binary_search_spec.rb
+++ spec/code/binary_search_spec.rb
@@ -1,25 +1,25 @@
 require 'binary_search/s1.rb'
+require 'byebug'
 
 RSpec.describe method(:binary_search) do
-  it 'searches for the target value' do
+  def bsearch(values, target)
+    values.bsearch_index {|x| target <=> x } || -1
+  end
+
+  it 'correctly searches for the target value' do
     arr1 = [1,2,3,4,5,6,7,8,9,10]
-    expect(binary_search(arr1, 0)).to eq(-1)
-    expect(binary_search(arr1, 1)).to eq(0)
-    expect(binary_search(arr1, 7)).to eq(6)
-    expect(binary_search(arr1, 11)).to eq(-1)
+    [0, 1, 7, 11].each do |target|
+      expect(binary_search(arr1, target)).to eq(bsearch(arr1, target))
+    end
 
     arr2 = [1,2,3,4,5,6,7,8,9,10,11]
-    expect(binary_search(arr2, 0)).to eq(-1)
-    expect(binary_search(arr2, 1)).to eq(0)
-    expect(binary_search(arr2, 11)).to eq(10)
-    expect(binary_search(arr2, 12)).to eq(-1)
+    [0, 1, 11, 12].each do |target|
+      expect(binary_search(arr1, target)).to eq(bsearch(arr1, target))
+    end
 
     arr3 = [1,4,5,6,7,8,9,10,21,27,57,68,87,98,100]
-    expect(binary_search(arr3, 0)).to eq(-1)
-    expect(binary_search(arr3, 1)).to eq(0)
-    expect(binary_search(arr3, 57)).to eq(10)
-    expect(binary_search(arr3, 98)).to eq(13)
-    expect(binary_search(arr3, 100)).to eq(14)
-    expect(binary_search(arr3, 101)).to eq(-1)
+    [0, 1, 57, 98, 100, 101].each do |target|
+      expect(binary_search(arr1, target)).to eq(bsearch(arr1, target))
+    end
   end
 end
