640649a10d92f5a7c2e5657cf2b8dfffbecc0249
Linmiao Xu <linmiao.xu@gmail.com>
2019-01-23 17:58:11 +0800

    Add a solution, test, and fixture for network delay time

--- code/problems/network_delay_time/s1.rb
+++ code/problems/network_delay_time/s1.rb
@@ -1,3 +1,12 @@
-def network_delay_time(graph, source_vertex)
-
+def network_delay_time(edges, n_vertices, source_vertex)
+  distances = Array.new(n_vertices, Float::INFINITY)
+  distances[source_vertex] = 0
+  n_vertices.times do
+    edges.each do |u, v, weight|
+      if distances[u] + weight < distances[v]
+        distances[v] = distances[u] + weight
+      end
+    end
+  end
+  distances.max
 end
---
+++ spec/problems/network_delay_time_spec.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+RSpec.describe 'network delay time problem' do
+  let(:fixture) { 'spec/fixtures/files/problems/network_delay_time.txt' }
+  let(:data) { open(fixture, 'r') { |f| f.read.split(/\n/) } }
+  let(:edges) { JSON.parse(data[0]) }
+  let(:n) { data[1].to_i }
+  let(:k) { data[2].to_i }
+  let(:answer) { 34 }
+
+  context 'Bellman-Ford algorithm' do
+    before { require 'network_delay_time/s1.rb' }
+
+    it 'finds the right time' do
+      expect(network_delay_time(edges, n, k)).to eq(answer)
+    end
+  end
+end
