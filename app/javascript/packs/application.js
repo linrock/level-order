3857e2d4c2d5e48c713a73f0871dfb410c79ffc0
Linmiao Xu <linmiao.xu@gmail.com>
2019-01-29 21:12:01 +0800

    Stop using highlight js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,26 +1,6 @@
 /* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-import hljs from 'highlight.js/lib/highlight';
-import javascript from 'highlight.js/lib/languages/javascript';
-import ruby from 'highlight.js/lib/languages/ruby';
-import 'highlight.js/styles/shades-of-purple.css'
-// import 'highlight.js/styles/atelier-sulphurpool-dark.css'
-
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('ruby', ruby);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('pre.hljs').forEach(code => {
-    hljs.highlightBlock(code)
-  });
-
   document.querySelectorAll('pre.highlight').forEach(code => {
     // highlight class/def and matching end tags
     code.querySelector(".k").classList.add("definition");
